b99eb75bf6ab3f56e0675f4b011b7f770380f6ed
wydwww <wydwww@outlook.com>
2016-10-14 13:23:51 +0800

    load data

--- aetrain.lua
+++ aetrain.lua
@@ -1,13 +1,29 @@
 local optim = require 'optim'
 local gnuplot = require 'gnuplot'
 local image = require 'image'
+dofile 'real_data.lua'
 
 -- Set up Torch
 print('Setting up')
 torch.setdefaulttensortype('torch.FloatTensor')
 torch.manualSeed(1)
 
--- Load train and test data
+classes = {'1','2','3','4','5','6','7','8','9','10','11'}
+trSize = 1540
+teSize = 390
+
+-- load train and test data
+setmetatable(trainData, 
+    {__index = function(t, i) 
+                    return {t.data[i], t.labels[i]} 
+                end}
+)
+
+setmetatable(testData,
+    {__index = function(t, i)
+                    return {t.data[i], t.labels[i]}
+                end}
+)
 
 
 -- Choose model to train
